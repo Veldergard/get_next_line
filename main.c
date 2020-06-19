0b7875ba431a3752a7efcc06ee62b4965a582208
olaurine <olaurine@student.21-school.ru>
2020-06-19 20:43:01 +0300

    main

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <fcntl.h>
+#include "get_next_line.h"
+#include <stdio.h>
+
+int main()
+{
+	char *line;
+	int fd = open("/home/olaurine/get_next_line/file", O_RDONLY);
+	while (get_next_line(fd, &line) == 1)
+	{
+		printf("%s\n", line);
+	}
+}
